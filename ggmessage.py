90a23f54879bfb17b16d7e0fa5cb896d57fa6284
Antonio Consiglio <77753494+AntonioConsiglio@users.noreply.github.com>
2023-01-22 09:59:31 +0100

    getting options browser headless

--- ggmessage.py
+++ ggmessage.py
@@ -1,11 +1,19 @@
 from utils import get_qrimage,create_driver
+from selenium.webdriver.support.wait import WebDriverWait
 
-def recall_qrcode(broswer:str = "Chrome"):
-    driver = create_driver(broswer)
+def recall_qrcode(broswer:str = "Chrome",open_head:bool =False) -> None:
+    '''
+    #Input:
+    - broswer: select your broswer from [chrome, firefox, edge, safari]
+    - open_head: if True the broswer page is opened
+    '''
+    driver = create_driver(broswer,open_head)
     driver.get("https://messages.google.com/web/authentication")
     qrcode,data = get_qrimage(driver)
     if qrcode is not None:
         qrcode.save("qrcode.png")
-        print(data)
+        # driver.get(data.decode('utf-8'))
+        # print(data)
+    driver.quit()
 
 
--- utils.py
+++ utils.py
@@ -1,32 +1,77 @@
 import selenium.webdriver as wd
 from selenium.webdriver.common.by import By
-from PIL import Image
+from PIL import Image,ImageOps
 from pyzbar.pyzbar import decode
 import io
 import time
 
 DRIVERS = {
-    "chrome": (lambda: wd.Chrome()),
-    "firefox": (lambda: wd.Firefox()),
-    "safari": (lambda: wd.Safari()),
-    "edge": (lambda: wd.Edge())
+    "chrome": (lambda options: wd.Chrome(options=options)),
+    "firefox": (lambda options: wd.Firefox(options=options)),
+    "safari": (lambda options: wd.Safari(options=options)),
+    "edge": (lambda options: wd.Edge(options=options))
 }
 
-def create_driver(brosware_name:str) -> wd.Chrome:
-    driver = DRIVERS[brosware_name.lower()]()
-    return driver
+#Options headless broswer
+CHROMEOPTIONS = wd.ChromeOptions()
+CHROMEOPTIONS.headless=True
+FIREFOXOPTIONS = wd.FirefoxOptions()
+FIREFOXOPTIONS.headless = True
+SAFARIOPTIONS = wd.FirefoxOptions()
+SAFARIOPTIONS.headless = True
+EDGEOPTIONS = wd.EdgeOptions()
+EDGEOPTIONS.headless = True
+
+OPTIONS ={
+	"chrome":CHROMEOPTIONS,
+	"firefox": FIREFOXOPTIONS,
+    "safari": SAFARIOPTIONS,
+    "edge": EDGEOPTIONS,
+	"headyes": None
+}
+
+# Custom Exceptions
+
+class BrowserException(Exception):
+	def __init__(self):
+		super(BrowserException,self).__init__("Only [Chrome, Firefox, Safari, Edge] are supported")	
+
+class TimeOutConnectionException(Exception):
+	def __init__(self):
+		super(TimeOutConnectionException,self).__init__("Verify your internet connection")		
+
+# functions
+
+def create_driver(brosware_name:str,open_head:bool) -> wd.Chrome:
+	try:
+		if not open_head:
+			driver = DRIVERS[brosware_name.lower()](options = OPTIONS[brosware_name.lower()])
+			return driver
+		driver = DRIVERS[brosware_name.lower()](options = OPTIONS["headyes"])
+	except KeyError as e:
+		raise(BrowserException())
+	return driver
 
 
-def get_qrimage(driver):
-    time.sleep(2)
-    element = driver.find_elements(By.CLASS_NAME,"bigger-qr-code")
-    qrcode  = Image.open(io.BytesIO(element[0].screenshot_as_png))
-    data = decode(qrcode)
-    if data:
-        data = data[0]
-        qrcode = qrcode.crop((data.rect.left,
-                                data.rect.top,
-                                data.rect.width+data.rect.left,
-                                data.rect.height+data.rect.top))
-        return qrcode,data.data
-    return None,None
+def get_qrimage(driver:wd.Chrome) -> list([Image,bytes]):
+	'''
+		The function return the qrcode image in PIL format and \n
+		the bytes massege inside the qr code
+	'''
+	element, start = [], time.time()
+	while len(element) == 0:
+		element = driver.find_elements(By.CLASS_NAME,"bigger-qr-code")
+		if time.time() - start > 10:
+			raise TimeOutConnectionException()
+	qrcode  = Image.open(io.BytesIO(element[0].screenshot_as_png))
+	data = decode(qrcode)
+	if data:
+		data = data[0]
+		qrcode = qrcode.crop((data.rect.left,
+								data.rect.top,
+								data.rect.width+data.rect.left,
+								data.rect.height+data.rect.top))
+		qrcode = ImageOps.equalize(ImageOps.grayscale(qrcode))
+		qrcode = ImageOps.expand(qrcode,border=50,fill="white")
+		return qrcode,data.data
+	return None,None
